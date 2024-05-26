79f9be453ee038b06de44acc7b8007c45b0990ae
Olawale Oyeyipo <olaclassik@gmail.com>
2024-05-26 11:46:43 +0100

    Before removing nightfox theme

--- init.lua
+++ init.lua
@@ -1,10 +1,2 @@
-:set number
-:set relativenumber
-:set autoindent
-:set tabstop=4
-:set smarttab
-:set softtabstop=4
-:set mouse=a
-
-
+require('config')
 
---
+++ lua/plugins/nightfox.lua
@@ -0,0 +1,8 @@
+return {
+	"EdenEast/nightfox.nvim",
+	lazy = false,
+	priority = 300,
+	config = function()
+		vim.cmd('colorscheme nightfox')	
+	end
+} 
