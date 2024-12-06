672255d2b8c3d735f7d67053fa19648296d9da99
Olawale Oyeyipo <olaclassik@gmail.com>
2024-12-06 05:57:53 +0100

    space things out

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,4 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
@@ -9,6 +10,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 		lazypath,
 	})
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require("config.globals")
@@ -42,3 +44,4 @@ local opts = {
 }
 
 require("lazy").setup("plugins", opts)
+
