8a7456118e764af9669305803db34344ee706c1c
Olawale Oyeyipo <olaclassik@gmail.com>
2024-12-15 17:57:06 +0100

    add more navigation keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,13 +1,17 @@
 local k = vim.keymap
 local opts = { noremap = true, silent = true }
 
-k.set("n", "<leader>pv", vim.cmd.Ex, opts)
+-- Clear highlights on search when pressing <Esc> in normal mode
+k.set('n', '<Esc>', '<cmd>nohlsearch<CR>', opts)
+
+-- Diagnostic keymaps
+k.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list'})
 
 k.set("v", "J", "<cmd>m '>+1<CR>gv=gv", opts)
 k.set("v", "K", "<cmd>m '<-2<CR>gv=gv", opts)
 
 -- greatest remap ever
---k.set("x", "<leader>p", [["_dP]], opts)
+k.set("x", "<leader>p", [["_dP]], opts)
 
 -- DISABLE arrow keys in normal mode
 k.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -31,6 +35,27 @@ k.set({'i', 'n'}, '<A-z>', function()
     end
 end)
 
-k.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode'})
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+k.set('t', '<Esc><Esc>', '<C-\\><C-n>', table.insert(opts, { desc = 'Exit terminal mode'}))
 
+-- Neo-tree navigation
+k.set("n", "<leader>m", ":Neotree toggle<CR>", table.insert(opts, { desc = 'Open file [M]anager'}))   
+
+--[[
+    /   <cmd>Neotree toggle current reveal_force_cwd<cr>
+    nnoremap | :Neotree reveal<cr>
+    nnoremap gd :Neotree float reveal_file=<cfile> reveal_force_cwd<cr>
+    nnoremap <leader>b :Neotree toggle show buffers right<cr>
+    nnoremap <leader>s :Neotree float git_status<cr>
+--]]
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
