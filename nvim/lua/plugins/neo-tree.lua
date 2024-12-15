7fef61a34d6cf9896f6e03dc480e8df369019838
Olawale Oyeyipo <olaclassik@gmail.com>
2024-12-15 11:12:47 +0100

    make hidden dot files visible

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -6,7 +6,24 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", 
       "MunifTanjim/nui.nvim",
-    }
+    },
+    config = function()
+        require("neo-tree").setup({
+            close_if_last_window = true,
+            window = {
+                position = "right",
+                width = 40,
+            },
+            filesystem = {
+                filtered_items = {
+                    visible = false, -- when true, they will just be displayed differently than normal items
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                    hide_hidden = false, -- only works on Windows for hidden files/directories
+                },
+            },
+        })
+    end
 }
 --[[
     nnoremap / :Neotree toggle current reveal_force_cwd<cr>
