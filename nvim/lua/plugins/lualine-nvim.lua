5d43b95b3d7ba2d9976e511e025cd9bbbe9d9258
Olawale Oyeyipo <olaclassik@gmail.com>
2024-12-16 18:15:27 +0100

    add lualine plugin

---
+++ nvim/lua/plugins/lualine-nvim.lua
@@ -0,0 +1,16 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    lazy = false,
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+        options = {
+            globalstatus = true,
+        },
+        sections = {
+            lualine_c = {
+                { 'buffers' },
+            }
+        }
+    }
+}
+
