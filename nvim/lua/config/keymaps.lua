fc9c99868c7afc3596245ce6b2da0d826743a4ec
Olawale Oyeyipo <olaclassik@gmail.com>
2024-12-17 18:20:38 +0100

    update configs

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,10 +2,10 @@ local k = vim.keymap
 local opts = { noremap = true, silent = true }
 
 -- Clear highlights on search when pressing <Esc> in normal mode
-k.set('n', '<Esc>', '<cmd>nohlsearch<CR>', opts)
+k.set("n", "<Esc>", "<cmd>nohlsearch<CR>", opts)
 
 -- Diagnostic keymaps
-k.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list'})
+k.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Remap J and K in visual mode to move selected lines down or up while maintaining selection and reindenting.
 k.set("v", "J", "<cmd>m '>+1<CR>gv=gv", opts)
@@ -16,8 +16,8 @@ k.set("n", "<C-d>", "<C-d>zz", opts)
 k.set("n", "<C-u>", "<C-u>zz", opts)
 
 -- Jump to the next or previous search result, center it, and open folds
-k.set("n", "n", "nzzzv", opts)  
-k.set("n", "N", "Nzzzv", opts) 
+k.set("n", "n", "nzzzv", opts)
+k.set("n", "N", "Nzzzv", opts)
 
 -- Perform custom line join operation
 k.set("n", "J", "mzJ`z", opts)
@@ -27,34 +27,41 @@ k.set("x", "<leader>p", [["_dP]], opts)
 
 -- For when you want to separate neovim and system clipboard
 -- Yank (copy) to system clipboard in normal and visual modes.
--- vim.keymap.set({"n", "v"}, "<leader>y", [["+y]]) 
+-- vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 -- Yank (copy) the entire line to system clipboard in normal mode.
 -- vim.keymap.set("n", "<leader>Y", [["+Y]])
 
--- Delete to the black hole register in normal and visual modes, without affecting the clipboard
--- normal `d` is CUT while the below is real delete deal
-k.set({"n", "v"}, "<leader>d", [["_d]], opts)
+-- Delete to the black hole register in normal and visual modes,
+-- without affecting the clipboard.
+-- Normal `d` is CUT while the below is real delete deal
+k.set({ "n", "v" }, "<leader>d", [["_d]], opts)
 
 -- Jump to the next or previous item in the quickfix list and center the cursor.
 -- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 -- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 
 -- Jump to the next or previous item in the location list and center the cursor.
--- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz") 
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 -- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-
 -- Search and replace the word under the cursor across the entire file, with confirmation prompt.
-k.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "[S]earch and [R]eplace current word" }) 
+k.set(
+	"n",
+	"<leader>sr",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "[S]earch and [R]eplace current word" }
+)
 
 -- Source current neovim config file
-k.set("n", "<leader>so", function() vim.cmd("so") end, { desc = "[S][O]urce current config file" })
+k.set("n", "<leader>so", function()
+	vim.cmd("so")
+end, { desc = "[S][O]urce current config file" })
 
 -- DISABLE arrow keys in normal mode
-k.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-k.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-k.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-k.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+k.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+k.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+k.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+k.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
 -- Exit on kj and jk
 k.set("i", "jj", "<Esc>", opts)
@@ -62,37 +69,51 @@ k.set("i", "jk", "<Esc>", opts)
 
 -- Toggle wrapping !!! WORKING BUT DO NOT like it yet
 local wrap = true
-k.set({'i', 'n'}, '<A-z>', function()
-    if wrap then
-        vim.cmd('set nowrap')
-        wrap = false
-    else
-        vim.cmd('set wrap')
-        wrap = true
-    end
+k.set({ "i", "n" }, "<A-z>", function()
+	if wrap then
+		vim.cmd("set nowrap")
+		wrap = false
+	else
+		vim.cmd("set wrap")
+		wrap = true
+	end
 end)
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-k.set('t', '<Esc><Esc>', '<C-\\><C-n>', table.insert(opts, { desc = 'Exit terminal mode'}))
+k.set("t", "<Esc><Esc>", "<C-\\><C-n>", table.insert(opts, { desc = "Exit terminal mode" }))
 
 -- Neo-tree navigation
-k.set("n", "\\", ":Neotree toggle<CR>", table.insert(opts, { desc = 'Neotree toggle'}))   
+-- TODO: create map for floating window bind to <leader>fm
+k.set("n", "\\", ":Neotree toggle<CR>", table.insert(opts, { desc = "Neotree toggle" }))
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
-k.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-k.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-k.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-k.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+k.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+k.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+k.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+k.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
---
+-- Indenting
+k.set("v", "<", "<gv")
+k.set("v", ">", ">gv")
 
---[[ NOTE: Find this to quite interesting
+--[[ NOTE: Find this to quite interesting bind <leader>z to quit, <leader><leader> to write
 mode = { "n", "v" },
     { "<leader>q", "<cmd>q<cr>", desc = "Quit" },
     { "<leader>w", "<cmd>w<cr>", desc = "Write" },
---]]
 
+:bd will close the current buffer.
+
+If you’re copying blocks of text around and need to align the indent 
+of a block in its new location, use ]p instead of just p.
+
+To mark a block of lines and indent it, Vjj> to indent three lines (Vim only).
+
+To indent the current line or a visual block:
+ctrl-t, ctrl-d  - indent current line forward, backwards 
+                  (insert mode)
+visual > or <   - indent block by sw (repeat with . )
+--]]
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,7 +3,7 @@ local opt = vim.opt
 
 -- Appearance
 opt.number = true
-opt.relativenumber = true	-- To help with jumping
+opt.relativenumber = true -- To help with jumping
 opt.termguicolors = true
 opt.colorcolumn = "80"
 opt.signcolumn = "yes"
@@ -12,7 +12,7 @@ opt.cmdheight = 1
 opt.completeopt = "menuone,noinsert,noselect"
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-opt.mouse = 'a'
+opt.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
 opt.showmode = false
@@ -21,7 +21,7 @@ opt.showmode = false
 -- Schedule the setting after `UiEnter` because it can increase startup-time.
 -- Remove this option if you want your OS clipboard to remain independent.
 vim.schedule(function()
-	opt.clipboard = 'unnamedplus'
+	opt.clipboard = "unnamedplus"
 end)
 
 -- Enable break indent
@@ -30,7 +30,7 @@ opt.breakindent = true
 -- Save undo history
 opt.undofile = true
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the 
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the
 -- search term
 opt.ignorecase = true
 opt.smartcase = true
@@ -50,10 +50,10 @@ opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor
 opt.list = true
-opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview Substitution live, as you type!
-opt.inccommand = 'split'
+opt.inccommand = "split"
 
 -- Show which line your cursor is on
 opt.cursorline = true
@@ -80,4 +80,3 @@ opt.iskeyword:append("-")
 opt.modifiable = true
 -- makes the cursor blinks but there is no diff btw normal and insert mode cursor
 --opt.guicursor = "n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
-
