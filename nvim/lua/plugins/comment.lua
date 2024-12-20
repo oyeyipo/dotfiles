f0e898a3c0682c795c2ce9b5ba5fc08b34b2485c
Olawale Oyeyipo <olaclassik@gmail.com>
2024-12-20 23:52:25 +0100

    add doc for keybinds

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,4 +1,28 @@
+-- NOTE: default keybindings from doc
+--
+-- toggler = {
+--     ---Line-comment toggle keymap
+--     line = 'gcc',
+--     ---Block-comment toggle keymap
+--     block = 'gbc',
+-- },
+-- ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+-- opleader = {
+--     ---Line-comment keymap
+--     line = 'gc',
+--     ---Block-comment keymap
+--     block = 'gb',
+-- },
+-- ---LHS of extra mappings
+-- extra = {
+--     ---Add comment on the line above
+--     above = 'gcO',
+--     ---Add comment on the line below
+--     below = 'gco',
+--     ---Add comment at the end of line
+--     eol = 'gcA',
+-- },
 return {
-    'numToStr/Comment.nvim',
+	"numToStr/Comment.nvim",
+	opts = {},
 }
-
