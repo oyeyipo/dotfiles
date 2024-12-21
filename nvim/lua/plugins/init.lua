b2f69035c4974dea84466fe2ed0ec166b326bd7a
Olawale Oyeyipo <olaclassik@gmail.com>
2024-12-21 22:19:01 +0100

    plugins formatter and update

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,16 +1,23 @@
 return {
-    { "folke/neoconf.nvim",  cmd = "Neoconf", opts = {} },
-    {
-        -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-        -- used for completion, annotations and signatures of Neovim apis
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {
-            library = {
-                -- Load luvit types when the `vim.uv` word is found
-                { path = "luvit-meta/library", words = { "vim%.uv" } },
-            },
-        },
-    },
-    { "Bilal2453/luvit-meta" },
+	{ "folke/neoconf.nvim", cmd = "Neoconf", opts = {} },
+	{
+		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+		-- used for completion, annotations and signatures of Neovim apis
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+
+				-- Only load the lazyvim library when the `LazyVim` global is found
+				{ path = "LazyVim", words = { "LazyVim" } },
+				-- Load the wezterm types when the `wezterm` module is required
+				-- Needs `justinsgithub/wezterm-types` to be installed
+				{ path = "wezterm-types", mods = { "wezterm" } },
+			},
+		},
+	},
+	{ "Bilal2453/luvit-meta" },
+	{ "justinsgithub/wezterm-types" },
 }
