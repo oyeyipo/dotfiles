0de1e76affc922df6c19faf1251de980d52cca61
Olawale Oyeyipo <olaclassik@gmail.com>
2024-12-25 22:52:21 +0100

    add telescope extension to lazygit to manage multiple github repos in one

--- nvim/lua/plugins/lazygit-nvim.lua
+++ nvim/lua/plugins/lazygit-nvim.lua
@@ -12,11 +12,22 @@ return { -- Calling lazygit from with neovim
 		"LazyGitFilterCurrentFile",
 	},
 	dependencies = {
+		"nvim-telescope/telescope.nvim",
 		"nvim-lua/plenary.nvim",
 	},
 	-- Setting the keybinding for LazyGit with 'keys' is recommended in
 	-- order to load the plugin whn the command is run for the first time
 	keys = {
 		{ "<leader>lg", "<cmd>LazyGit<CR>", desc = "Start [L]azy[G]it" },
+		{
+			"<leader>lt",
+			function()
+				require("telescope").extensions.lazygit.lazygit()
+			end,
+			desc = "Start [L]azygit [Telescope]",
+		},
 	},
+	config = function()
+		require("telescope").load_extension("lazygit")
+	end,
 }
