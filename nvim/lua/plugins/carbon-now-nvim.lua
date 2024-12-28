dad2fd8d3b24c1714289f16a93905d444ee11b3a
Olawale Oyeyipo <olaclassik@gmail.com>
2024-12-28 07:05:03 +0100

    add carbon-now plugin to capture code snippet

---
+++ nvim/lua/plugins/carbon-now-nvim.lua
@@ -0,0 +1,25 @@
+return { -- Create beautiful code snippet
+	"ellisonleao/carbon-now.nvim",
+	cmd = "CarbonNow",
+	keys = {
+		{
+			"<leader>cc",
+			"<cmd>CarbonNow<CR>",
+			{ desc = "[C]apture [C]ode", silent = true },
+			mode = "v",
+		},
+	},
+	opts = {
+		options = {
+			bg = "yellow",
+			theme = "Night Owl",
+			font_family = "Cascadia Code",
+			font_size = "16px",
+			titlebar = "Made with 💗 by Wale",
+			drop_shadow = true,
+			padding_horizontal = "40px",
+			padding_vertical = "40px",
+			window_theme = "none",
+		},
+	},
+}
