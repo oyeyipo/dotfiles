44ce567c87447cd79efdc1c643ed3447628d1f5b
Olawale Oyeyipo <olaclassik@gmail.com>
2024-12-31 16:17:23 +0100

    minor fixes

--- nvim/lua/plugins/markdown-preview-nvim.lua
+++ nvim/lua/plugins/markdown-preview-nvim.lua
@@ -5,6 +5,7 @@ return {
 	build = "cd app && npm install",
 	init = function()
 		vim.g.mkdp_filetypes = { "markdown" }
+		vim.g.mkdp_auto_close = 0
 	end,
 	config = function()
 		vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<CR>", { desc = "[M]arkdown [P]review" })
