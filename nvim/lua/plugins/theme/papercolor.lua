6462f8733fdc91d5cbdb9fd9d43c4d6f1e4d11c5
Olawale Oyeyipo <olaclassik@gmail.com>
2025-01-06 13:20:43 +0100

    add papercolor-theme

---
+++ nvim/lua/plugins/theme/papercolor.lua
@@ -0,0 +1,9 @@
+return {
+	"NLKNguyen/papercolor-theme",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.cmd([[set background=light]])
+		vim.cmd([[colorscheme papercolor]])
+	end,
+}
