306784999fffd5d0c81cfab15763765c7eafe750
Olawale Oyeyipo <olaclassik@gmail.com>
2025-01-31 08:40:34 +0100

    improve papercolor configuration

--- nvim/lua/plugins/theme/init.lua
+++ nvim/lua/plugins/theme/init.lua
@@ -4,6 +4,6 @@ return {
 
 	require("plugins.theme.nvim-catppuccin"),
 	require("plugins.theme.onedark"),
-	require("plugins.theme.night-owl"),
 	require("plugins.theme.papercolor"),
+	require("plugins.theme.night-owl"),
 }
--- nvim/lua/plugins/theme/papercolor.lua
+++ nvim/lua/plugins/theme/papercolor.lua
@@ -1,9 +1,24 @@
-return {
+return { -- Best light themed color
 	"NLKNguyen/papercolor-theme",
 	-- lazy = false,
 	-- priority = 1000,
 	config = function()
+		vim.g.PaperColor_Theme_Options = {
+			theme = {
+				default = {
+					override = {
+						color00 = { "#FFFFFF" },
+						color03 = { "#228B22" },
+						color06 = { "#001080" },
+						color10 = { "#267f99" },
+						color11 = { "#AF00DB" },
+						linenumber_fg = { "#59636e" },
+						linenumber_bg = { "#FFFFFF" },
+					},
+				},
+			},
+		}
 		vim.cmd([[set background=light]])
-		vim.cmd([[colorscheme papercolor]])
+		vim.cmd("colorscheme PaperColor")
 	end,
 }
