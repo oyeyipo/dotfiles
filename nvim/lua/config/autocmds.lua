35041ecd1525b9567e9e53e31a35e9ef57c82dfb
Olawale Oyeyipo <olaclassik@gmail.com>
2025-02-08 09:22:25 +0100

    solve lsp formatting error and format file properly

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,35 +1,24 @@
--- Format on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-    desc = "Format on save",
-	group = vim.api.nvim_create_augroup("olawale-lsp-formatting", {}),
-	callback = function()
-		-- local efm = vim.lsp.get_active_clients({ name = "efm" })
-		local efm = vim.lsp.get_clients()
-		if vim.tbl_isempty(efm) then
-			return
-		end
-
-		vim.lsp.buf.format({ name = "efm" })
-	end,
-})
+-- Format on save: Based on `efm` lsp, not required if it doesn't exist
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+-- 	desc = "Format on save",
+-- 	group = vim.api.nvim_create_augroup("olawale-lsp-formatting", {}),
+-- 	callback = function()
+-- 		-- local efm = vim.lsp.get_active_clients({ name = "efm" })
+-- 		local efm = vim.lsp.get_clients()
+-- 		if vim.tbl_isempty(efm) then
+-- 			return
+-- 		end
+--
+-- 		vim.lsp.buf.format({ name = "efm" })
+-- 	end,
+-- })
 
 -- Highlight when yanking (copying) text
 -- Try it with `yap` in normal mode
-vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	group = vim.api.nvim_create_augroup('olawale-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("olawale-highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
 })
-
--- Remove background
---[[
-vim.cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-]]
---]]
-
