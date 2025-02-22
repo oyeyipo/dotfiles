77f44d2d4515ebbe401fcd987e736a753d35edee
Olawale Oyeyipo <olaclassik@gmail.com>
2025-02-22 13:16:04 +0100

    proper setup oil.nvim with keybinding

--- nvim/lua/plugins/oil-nvim.lua
+++ nvim/lua/plugins/oil-nvim.lua
@@ -1,9 +1,10 @@
 return {
 	"stevearc/oil.nvim",
-	opts = {},
-	-- Optional dependencies
+	lazy = false,
+	config = function()
+		require("oil").setup()
+
+		vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+	end,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	-- dependencies = { { "echasnovski/mini.icons", opts = {} } }, -- use if you prefer nvim-web-devicons
-	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-	-- lazy = false,
 }
