c198c40aed5c533d8a641aff1153ae7cf1f0760e
Olawale Oyeyipo <olaclassik@gmail.com>
2025-04-29 19:16:22 +0100

    Added useful comments from kickstart.nvim

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -2,7 +2,22 @@ return {
 	"echasnovski/mini.nvim",
 	version = false,
 	config = function()
-		require("mini.ai").setup()
+		-- Better Around/Inside textobjects
+		--
+		-- Examples:
+		--  - va) - [V]isually select [A]round [)]paren
+		--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+		--  - ci'  - [C]hange [I]nside [']quote
+		require("mini.ai").setup({ n_lines = 500 })
+
+		-- Add/delete/replace surroundings (brackets, quotes, etc.)
+		--
+		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+		-- - sd'   - [S]urround [D]elete [']quotes
+		-- - sr)'  - [S]urround [R]eplace [)] [']
 		require("mini.surround").setup()
+
+		-- there is more
+		-- Check out: https://github.com/echasnovski/mini.nvim
 	end,
 }
