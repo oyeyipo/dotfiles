f2f302949824cf2eb53d433fc047ff0365941e26
Olawale Oyeyipo <olaclassik@gmail.com>
2025-12-17 20:46:13 +0100

    round pane corners

--- nvim/.config/nvim/lua/plugins/navigator.lua
+++ nvim/.config/nvim/lua/plugins/navigator.lua
@@ -1,14 +1,12 @@
 return {
-	"numToStr/Navigator.nvim",
-	event = "VimEnter",
-	config = function()
-		require("Navigator").setup({})
-
-		-- TODO: Keybinding
-		-- vim.keymap.set({ "n", "t" }, "<C-h>", ":NavigatorLeft<CR>", { desc = "Move focus to the left window" })
-		-- vim.keymap.set({ "n", "t" }, "<C-l>", ":NavigatorRight<CR>", { desc = "Move focus to the right window" })
-		-- vim.keymap.set({ "n", "t" }, "<C-k>", ":NavigatorUp<CR>", { desc = "Move focus to the up window" })
-		-- vim.keymap.set({ "n", "t" }, "<C-j>", ":NavigatorDown<CR>", { desc = "Move focus to the down window" })
-		-- vim.keymap.set({ "n", "t" }, "<C-p>", "<CMD>NavigatorPrevious<CR>")
-	end,
+	"swaits/zellij-nav.nvim",
+	lazy = true,
+	event = "VeryLazy",
+	keys = {
+		{ "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>", { silent = true, desc = "navigate left or tab" } },
+		{ "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+		{ "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+		{ "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right or tab" } },
+	},
+	opts = {},
 }
