ea5aade9f9919c48463e2c66771801f6d792c88c
Olawale Oyeyipo <olaclassik@gmail.com>
2026-01-30 18:35:44 +0100

    move Telescope config to be contained by plugin

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return { -- Fuzzy Finder (files, lsp, etc)
 	"nvim-telescope/telescope.nvim",
 	event = "VimEnter",
-	tag = "v0.2.0",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -81,6 +80,62 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 		vim.keymap.set("n", "<leader>sc", builtin.commands, { desc = "[S]earch [C]ommands" })
 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+		--  This function gets run when an LSP attaches to a particular buffer.
+		--    That is to say, every time a new file is opened that is associated with
+		--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
+		--    function will be executed to configure the current buffer
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("olawale-lsp-attach", { clear = true }),
+			callback = function(event)
+				local buf = event.buf
+
+				-- Jump to the definition of the word under your cursor.
+				--  This is where a variable was first declared, or where a function is defined, etc.
+				--  To jump back, press <C-t>
+				vim.keymap.set("n", "grd", builtin.lsp_definitions, { buffer = buf, desc = "[G]oto [D]efinition" })
+
+				-- Find references for the word under your cursor.
+				vim.keymap.set("n", "grr", builtin.lsp_references, { buffer = buf, desc = "[G]oto [R]eferences" })
+
+				-- Jump to the implementation of the word under your cursor.
+				--  Useful when your language has ways of declaring types without an actual implementation.
+				vim.keymap.set(
+					"n",
+					"gri",
+					builtin.lsp_implementations,
+					{ buffer = buf, desc = "[G]oto [I]mplementation" }
+				)
+
+				-- Fuzzy find all the symbols in your current document.
+				--  Symbols are things like variables, functions, types, etc.
+				vim.keymap.set(
+					"n",
+					"gO",
+					builtin.lsp_document_symbols,
+					{ buffer = buf, desc = "Open Document Symbols" }
+				)
+
+				-- Fuzzy find all the symbols in your current workspace.
+				--  Similar to document symbols, except searches over your entire project.
+				vim.keymap.set(
+					"n",
+					"gW",
+					builtin.lsp_dynamic_workspace_symbols,
+					{ buffer = buf, desc = "Open Workspace Symbols" }
+				)
+
+				-- Jump to the type of the word under your cursor.
+				--  Useful when you're not sure what type a variable is and you want to see
+				--  the definition of its "type", not where it was "defined".
+				vim.keymap.set(
+					"n",
+					"grt",
+					builtin.lsp_type_definitions,
+					{ buffer = buf, desc = "[G]oto [T]ype Definition" }
+				)
+			end,
+		})
 
 		vim.keymap.set("n", "<leader>/", function()
 			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
