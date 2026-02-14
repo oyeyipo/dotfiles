ef942c06a4f63eecd7c89d34769f4e70976fa6b2
Olawale Oyeyipo <olaclassik@gmail.com>
2026-02-14 15:46:43 +0100

    theme adjustments

--- nvim/.config/nvim/lua/plugins/lualine-nvim.lua
+++ nvim/.config/nvim/lua/plugins/lualine-nvim.lua
@@ -1,32 +1,33 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	lazy = false,
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	-- opts = {},
-	config = function()
-		-- require("transparent").clear_prefix("lualine")
+  'nvim-lualine/lualine.nvim',
+  lazy = false,
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  -- opts = {},
+  config = function()
+    -- require("transparent").clear_prefix("lualine")
 
-		-- require('lspconfig').util.search_ancestors
+    -- require('lspconfig').util.search_ancestors
 
-		-- local lint_progress = function()
-		-- 	local linters = require("lint").get_running()
-		-- 	if #linters == 0 then
-		-- 		return "󰦕"
-		-- 	end
-		-- 	return "󱉶 " .. table.concat(linters, ", ")
-		-- end
+    -- local lint_progress = function()
+    -- 	local linters = require("lint").get_running()
+    -- 	if #linters == 0 then
+    -- 		return "󰦕"
+    -- 	end
+    -- 	return "󱉶 " .. table.concat(linters, ", ")
+    -- end
 
-		require("lualine").setup({
-			options = {
-				globalstatus = true,
-			},
-			sections = {
-				lualine_c = {
-					"filename",
-					"lsp_status",
-					-- lint_progress,
-				},
-			},
-		})
-	end,
+    require('lualine').setup {
+      theme = 'night-owl',
+      options = {
+        globalstatus = true,
+      },
+      sections = {
+        lualine_c = {
+          'filename',
+          'lsp_status',
+          -- lint_progress,
+        },
+      },
+    }
+  end,
 }
--- nvim/.config/nvim/lua/plugins/theme/night-owl.lua
+++ nvim/.config/nvim/lua/plugins/theme/night-owl.lua
@@ -1,12 +1,12 @@
 return {
-	"oxfist/night-owl.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("night-owl").setup({
-			italics = false,
-			transparent_background = vim.g.transparent_enabled,
-		})
-		vim.cmd([[colorscheme night-owl]])
-	end,
+  'oxfist/night-owl.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('night-owl').setup {
+      italics = true,
+      transparent_background = vim.g.transparent_enabled,
+    }
+    vim.cmd [[colorscheme night-owl]]
+  end,
 }
