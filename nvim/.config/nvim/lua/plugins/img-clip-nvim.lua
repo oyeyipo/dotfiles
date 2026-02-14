a8adcbe072900b5db3a6e15a97d0b84e8908a377
Olawale Oyeyipo <olaclassik@gmail.com>
2026-02-14 16:31:48 +0100

    image pasting capability

---
+++ nvim/.config/nvim/lua/plugins/img-clip-nvim.lua
@@ -0,0 +1,8 @@
+return {
+  'HakonHarnes/img-clip.nvim',
+  event = 'VeryLazy',
+  opts = {},
+  keys = {
+    { '<leader>im', '<cmd>PasteImage<cr>', desc = 'Paste [I][m]age from system clipboard' },
+  },
+}
