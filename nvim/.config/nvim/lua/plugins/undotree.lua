80b2666110913e47f3d5f9b71fcc582a0c191555
Olawale Oyeyipo <olaclassik@gmail.com>
2026-04-11 18:01:37 +0100

    format file

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -1,7 +1,5 @@
 return {
-	"mbbill/undotree",
-	event = "BufWinEnter",
-	config = function()
-		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle [U]ndo Tree" })
-	end,
+  'mbbill/undotree',
+  event = 'BufWinEnter',
+  config = function() vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Toggle [U]ndo Tree' }) end,
 }
