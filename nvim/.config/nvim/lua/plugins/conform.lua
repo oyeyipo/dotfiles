a212a02760888f2285109dbd1160b56101aff24a
Olawale Oyeyipo <olaclassik@gmail.com>
2026-04-27 15:46:12 +0100

    format conform file

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,77 +1,72 @@
 return { -- Autoformat
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>f",
-			function()
-				require("conform").format({ async = true, lsp_format = "fallback" })
-			end,
-			mode = "",
-			desc = "[F]ormat buffer",
-		},
-	},
-	opts = {
-		notify_on_error = false,
-		format_on_save = function(bufnr)
-			-- Disable with a global or buffer-local variable
-			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-				return
-			end
-			-- Disable "format_on_save lsp_fallback" for languages that don't
-			-- have a well standardized coding style. You can add additional
-			-- languages here or re-enable it for the disabled ones.
-			local disable_filetypes = { c = true, cpp = true }
-			if disable_filetypes[vim.bo[bufnr].filetype] then
-				return nil
-			else
-				return {
-					timeout_ms = 3000,
-					lsp_format = "fallback",
-				}
-			end
-		end,
-		formatters_by_ft = {
-			lua = { "stylua" },
-			markdown = { "doctoc", "markdownlint", "mdformat" },
-			sh = { "shfmt" },
-			kdl = { "kdlfmt" },
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function() require('conform').format { async = true } end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
+      local disable_filetypes = { c = true, cpp = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return nil
+      else
+        return {
+          timeout_ms = 3000,
+        }
+      end
+    end,
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      markdown = { 'doctoc', 'markdownlint', 'mdformat' },
+      sh = { 'shfmt' },
+      kdl = { 'kdlfmt' },
 
-			-- Conform can also run multiple formatters sequentially
-			-- python = { "isort", "docformatter", "black" },
-			python = { "isort", "ruff_fix", "ruff_format", lsp_format = "never" },
-			htmldjango = { "djlint" },
+      -- Conform can also run multiple formatters sequentially
+      -- python = { "isort", "docformatter", "black" },
+      python = { 'isort', 'ruff_fix', 'ruff_format', lsp_format = 'never' },
+      htmldjango = { 'djlint' },
 
-			-- Web Techs
-			-- You can use 'stop_after_first' to run the first available formatter from the list
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-			html = { "prettierd", "prettier", stop_after_first = true },
-			css = { "prettierd", "prettier", stop_after_first = true },
-			typescript = { "prettierd", "prettier", stop_after_first = true },
-			yaml = { "prettierd", "prettier", stop_after_first = true },
+      -- Web Techs
+      -- You can use 'stop_after_first' to run the first available formatter from the list
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      html = { 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      yaml = { 'prettierd', 'prettier', stop_after_first = true },
 
-			-- Use the "*" filetype to run formatters on all filetypes.
-			["*"] = { "trim_whitespace" },
+      -- Use the "*" filetype to run formatters on all filetypes.
+      ['*'] = { 'trim_whitespace' },
 
-			-- Use the "_" filetype to run formatters on filetypes that don't
-			-- have other formatters configured.
-			["_"] = { "trim_whitespace" },
-		},
-		-- Custom formatters and overrides for built-in formatters
-		formatters = {
-			isort = {
-				command = "isort",
-				args = { "-" },
-			},
-			djlint = {
-				prepend_args = {
-					"--profile=django",
-					"--indent=2",
-					"--max-line-length=80",
-					"--max-blank-lines=1",
-				},
-			},
-		},
-	},
+      -- Use the "_" filetype to run formatters on filetypes that don't
+      -- have other formatters configured.
+      ['_'] = { 'trim_whitespace' },
+    },
+    default_format_opts = {
+      lsp_format = 'fallback', -- Use external formatters if configured below, otherwise use LSP formatting. Set to `false` to disable LSP formatting entirely.
+    },
+    -- Custom formatters and overrides for built-in formatters
+    formatters = {
+      isort = {
+        command = 'isort',
+        args = { '-' },
+      },
+      djlint = {
+        prepend_args = {
+          '--profile=django',
+          '--indent=2',
+          '--max-line-length=80',
+          '--max-blank-lines=1',
+        },
+      },
+    },
+  },
 }
