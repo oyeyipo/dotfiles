86338a632ad7e0045892f0dd6c27579d47f000a1
Olawale Oyeyipo <olaclassik@gmail.com>
2026-08-31 11:50:15 +0100

    fix error

--- omp_env.sh
+++ omp_env.sh
@@ -1,3 +1,3 @@
-# This script is symlinked to the directory: /etc/profile.d
-export POSH_THEMES_PATH=~/.cache/oh-my-posh/themes
+#!/bin/bash
 
+export POSH_THEMES_PATH=~/.cache/oh-my-posh/themes # This script is symlinked to the directory: /etc/profile.d
