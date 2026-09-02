471c7f7389787cd951b0b8a8f34870f3cc1ad041
Olawale Oyeyipo <olaclassik@gmail.com>
2026-09-02 09:14:32 +0100

    cleanup

--- nvim/.config/nvim/lua/plugins/theme/init.lua
+++ nvim/.config/nvim/lua/plugins/theme/init.lua
@@ -1,7 +1,6 @@
 -- If you want to see what colorscheme are already installed, you can use `:Telescope colorscheme`.
 return {
   require 'plugins.theme.catppuccin',
-  require 'plugins.theme.embark',
   require 'plugins.theme.night-fox',
   require 'plugins.theme.night-owl',
   require 'plugins.theme.tokyo-night',
