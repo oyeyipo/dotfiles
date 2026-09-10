beff23103d212527501ab199d36bff98f2a2edb2
Olawale Oyeyipo <olaclassik@gmail.com>
2026-09-10 13:39:35 +0100

    make modicator load immediately

--- nvim/.config/nvim/lua/plugins/modicator.lua
+++ nvim/.config/nvim/lua/plugins/modicator.lua
@@ -1,5 +1,7 @@
+-- Cursor line number mode indicator plugin
 return {
   'mawkler/modicator.nvim',
+  lazy = false,
   dependencies = 'oxfist/night-owl.nvim', -- Add your colorscheme plugin here
   init = function()
     -- These are required for Modicator to work
@@ -9,9 +11,6 @@ return {
   end,
   opts = {
     -- Warn if any required option above is missing. May emit false positives
-    -- if some other plugin modifies them, which in that case you can just
-    -- ignore. Feel free to remove this line after you've gotten Modicator to
-    -- work properly.
     show_warnings = true,
   },
 }
